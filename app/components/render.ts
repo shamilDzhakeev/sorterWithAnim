3262a1984b46509160f32c812a60900478e22dac
Shamil Dzhakeev <shamil.dzhakeev@ru.axxonsoft.com>
2019-05-22 11:55:00 +0300

    update method updateRender

--- app/components/render.ts
+++ app/components/render.ts
@@ -3,24 +3,25 @@ export default class Renderer {
   private index: number[];
   private container: HTMLDivElement;
 
-  public constructor(arr: number[], columnsContainer) {
+  public constructor(arr: number[], columnsContainer: HTMLDivElement) {
     const COLUMN_HEIGHT = 15;
 
     this.container = columnsContainer;
     this.curValuesArr = [...arr];
     this.index = [];
-    this.index = arr.map((_, i): number => i);
 
-    for (let i = 0; i < this.curValuesArr.length; i += 1) {
-      const newColumn = document.createElement('div');
+    this.curValuesArr.forEach(
+      (value, index): void => {
+        this.index.push(index);
 
-      newColumn.className = 'column';
-      newColumn.style.height = `${this.curValuesArr[i] * COLUMN_HEIGHT}px`;
-      newColumn.style.left = Renderer.getColumnOffset(i);
-      newColumn.innerText = this.curValuesArr[i].toString();
-
-      columnsContainer.appendChild(newColumn);
-    }
+        const newColumn = document.createElement('div');
+        newColumn.className = 'column';
+        newColumn.style.height = `${value * COLUMN_HEIGHT}px`;
+        newColumn.style.left = Renderer.getColumnOffset(index);
+        newColumn.innerText = value.toString();
+        columnsContainer.appendChild(newColumn);
+      },
+    );
   }
 
   private static COLUM_SPACING = 30;
@@ -30,23 +31,22 @@ export default class Renderer {
   }
 
   public updateRender(newValuesArr: number[]): void {
-    let tempIndex: number[] = [];
-    let changeFlag = false;
     const columns = this.container.getElementsByTagName('div');
+
     for (let i = 0; i < newValuesArr.length; i += 1) {
       if (newValuesArr[i] !== +this.curValuesArr[i]) {
-        tempIndex = [...this.index];
-        [tempIndex[i], tempIndex[i + 1]] = [tempIndex[i + 1], tempIndex[i]];
-        changeFlag = true;
+        [this.index[i], this.index[i + 1]] = [this.index[i + 1], this.index[i]];
         break;
       }
     }
-    if (changeFlag) {
-      this.index = tempIndex;
-    }
-    for (let i = 0; i < columns.length; i += 1) {
-      columns[this.index[i]].style.left = Renderer.getColumnOffset(i);
-    }
+
+    Array.prototype.forEach.call(
+      columns,
+      (_, i): void => {
+        columns[this.index[i]].style.left = Renderer.getColumnOffset(i);
+      },
+    );
+
     this.curValuesArr = newValuesArr;
   }
 }
